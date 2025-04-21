6567f16bb32abc9bf890863ae0a48853dfabcffb
Villa <108865128+villacapri@users.noreply.github.com>
2025-04-21 15:52:26 -0400

    Delete ApartmentSpotter/main.py

--- ApartmentSpotter/main.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-
-app = Flask(__name__, template_folder='apartment_reviews/templates')
-
-if not os.environ.get("SESSION_SECRET"):
-    os.environ["SESSION_SECRET"] = "apartment-reviews-secret-key"
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
